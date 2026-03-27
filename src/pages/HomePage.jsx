08677824a2c4ac7855c97a34f36af6cc297dd4da
Prom Kathintong <67160192@go.buu.ac.th>
2026-03-27 14:01:14 +0700

    feat: task4 - router, context, vercel spa routing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,14 @@
 import PostList from "../components/PostList";
 import AddPostForm from "../components/AddPostForm";
-import { useFavorites } from "../context/FavoritesContext"; // 1. นำเข้า Context
+import { useFavorites } from "../context/FavoritesContext"; 
 
 function HomePage() {
-  const { favorites, toggleFavorite } = useFavorites(); // 2. ดึงข้อมูลจาก Context
+  const { favorites, toggleFavorite } = useFavorites(); 
 
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto", padding: "0 1rem" }}>
       <AddPostForm onAddPost={() => {}} />
       
-      {/* 3. ส่งข้อมูล (Props) เข้าไปให้ PostList */}
       <PostList 
         favorites={favorites} 
         onToggleFavorite={toggleFavorite} 
